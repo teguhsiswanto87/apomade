e8a8973d9df69bb6885c270fe756f8be13486168
teguhsiswanto87 <teguhsiswanto87@gmail.com>
2019-12-22 13:26:49 +0700

    fix data dummy

--- public/assets/sql/data_dummy.sql
+++ public/assets/sql/data_dummy.sql
@@ -1,12 +1,12 @@
 insert into products(id, name , stock, capital, selling_price, gross_profit) values
-(null, 'pomade reguler', 19, 19000, 34000, 12000),
-(null, 'pomade platinum', 11, 11000, 64000, 92000),
-(null, 'pomade Extrem', 20, 19000, 34000, 12000),
-(null, 'pomade Hard', 7, 9000, 14000, 12000),
-(null, 'pomade Medium', 10, 10000, 34000, 32000),
-(null, 'pomade Easy', 50, 12000, 90000, 32000),
-(null, 'kaos polos', 5, 22000, 91000, 32000),
-(null, 'Jersey GO Timnas', 11, 12000, 90000, 32000);
+(null, 'pomade reguler', 19, 35000, 65000, 30000),
+(null, 'pomade platinum', 11, 40000, 70000, 30000),
+(null, 'pomade Extrem', 20, 45000, 75000, 30000),
+(null, 'pomade Hard', 7, 25000, 40000, 15000),
+(null, 'pomade Medium', 10, 30000, 43000, 13000),
+(null, 'pomade Easy', 50, 27000, 41000, 14000),
+(null, 'kaos polos', 5, 25000, 35000, 10000),
+(null, 'Jersey GO Timnas', 11, 45000, 60000, 15000);
 
 insert into couriers(id, name, active)values
 (null, 'JNE', 'Y'),
@@ -16,7 +16,7 @@ insert into couriers(id, name, active)values
 (null, 'Wahana', 'N');
 
 insert into market_places(id, name, active, image_link)values
-(null, 'bukalapak', 'Y', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rumahku.co.id%2Fimages%2Fbukalapak.png&f=1&nofb=1'),
+(null, 'Bukalapak', 'Y', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rumahku.co.id%2Fimages%2Fbukalapak.png&f=1&nofb=1'),
 (null, 'Shopee', 'Y','https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F236x%2F0f%2F06%2Fdc%2F0f06dc51a4ef42e672fbac51fdeea488.jpg&f=1&nofb=1'),
 (null, 'Tokopedia', 'N','https://files.startupranking.com/startup/thumb/3644_0ee72372e9a1418b95a6203eed53464d2c83b4c4_tokopedia_m.jpg'),
 (null, 'Instagram', 'Y','https://instagram-brand.com/wp-content/themes/ig-branding/assets/images/ig-logo-email.png'),
